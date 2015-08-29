06d966be7be97d8600ab88a323854b0c877416a5
Daniel Moennich <daniel.moennich@gmail.com>
2015-08-28 23:52:29 -0400

    Raw finish math eval

--- temp/evalmath/evalmath.js
+++ temp/evalmath/evalmath.js
@@ -1,24 +1,9 @@
 
-//You need to support multiple levels of nested parentheses, ex. (2 / (2 + 3.33) * 4) - -6
 
 
+// remove all whitespace
 var removeWS = function (exp) {
 	return exp.replace(/\s+/g, "");
-};
-
-var reduceOperators = function (exp) {
-
-	var evalOperators = function (operatorSequence) {
-		if (operatorSequence.length === 2) {
-			if (operatorSequence === "+-" || operatorSequence === "-+") {
-				return "-";
-			}
-			return "+";
-		}
-		return evalOperators(evalOperators(operatorSequence.substr(0,2)) + operatorSequence.substr(2));
-	};
-
-	return exp.replace(/[\+-]{2,}/g, evalOperators);
 };
 
 // reduce "multi operators", like
@@ -28,16 +13,62 @@ var reduceOperators = function (exp) {
 // 5 +++ - ++ 7 -> 5 - 7
 // 5 - -2 -> 5 + 2
 // 5 +-++ - + 7 -> 5 + 7
-// the above can be done with str.replace(regexForOperators, $1...)
-// special case is only one operand: -4, +5
-var getOpArr = function (exp) {
-	var result = /([^\*/]*)(\d+)([\*/])(\d+)(.*)/.exec(exp);	// first appearance of * or /
-	return result || /([\+-]*)(\d+)([\+-])(\d+)(.*)/.exec(exp);	// if no */, go for +-
+var reduceOperators = function (exp) {
+	var evalOperators = function (operatorSequence) {
+		if (operatorSequence.length === 2) {
+			if (operatorSequence === "+-" || operatorSequence === "-+") {
+				return "-";
+			}
+			return "+";	// ++, --
+		}
+		return evalOperators(evalOperators(operatorSequence.substr(0,2)) + operatorSequence.substr(2));
+	};
+	return exp.replace(/[\+-]{2,}/g, evalOperators);
 };
 
+//	1. fills in zeros for unary operators, e.g. +4 -> 0+4; (-6) -> (0-6)
+//	2. replaces single numbers to addition with zero, e.g. 42 -> 0+42
+var zeroFill = function (exp) {
+	exp = exp.replace(/(^|\()([-\+]\d+)/g, "$10$2");
+	return exp.replace(/^(\d+)$/, "0+$1");
+};
+
+//	creates an Array that around the expression with the highest prio
+//	[0] -> whole expression
+//	[1] -> string before 1st operand
+//	[2] -> 1st operand
+//	[3] -> operator
+//	[4] -> 2nd operand
+//	[5] -> string after 2nd operand
+//	Example: ["1+2*3-4", "1+", "2", "*", "3", "-4"]
+var getOpArr = function (exp) {
+	// decimal number: (\d+(?:\.\d+)?)
+	// ([^\*/]*?)(\d+(?:\.\d+)?)([\*/])(\d+(?:\.\d+)?)(.*)
+	// ([^\*/]*)(\d+)([\*/])(\d+)(.*)
+	var result = /([^\*/]*?)(\d+(?:\.\d+)?)([\*/])(\d+(?:\.\d+)?)(.*)/.exec(exp);	// first appearance of * or /
+	
+	// ([\+-]*)(\d+(?:\.\d+)?)*([\+-])(\d+(?:\.\d+)?)+(.*)
+	// ([\+-]*)(\d*)([\+-])(\d+)(.*)
+	return result || /([\+-]*)(\d+(?:\.\d+)?)*([\+-])(\d+(?:\.\d+)?)+(.*)/.exec(exp);	// if no */, go for +-
+};
+
+var solveParenthesis = function (exp) {
+	var parentArr = /(.*?)(\([^\(]*?\))(.*)/.exec(exp);
+	if (!parentArr) {
+		return exp;
+	}
+	var parentResult = calc(parentArr[2].slice(1, -1));
+	return solveParenthesis(parentArr[1] + parentResult + parentArr[3]);
+};
 
 var calc = function (exp) {
+	if (!exp) {
+		return null;
+	}
 	exp = removeWS(exp);
+	exp = reduceOperators(exp);
+	exp = solveParenthesis(exp);
+	exp = zeroFill(exp);
 	var opArr = getOpArr(exp),
 	result = doArith(opArr[2], opArr[3], opArr[4]);
 	if (opArr[1] === "" && opArr[5] === "") {
--- temp/evalmath/evalmathSpec.js
+++ temp/evalmath/evalmathSpec.js
@@ -2,31 +2,8 @@ describe("removeWS", function () {
 	it("should replace all white space from expression", function () {
 		expect(removeWS("1 + 3-4  *  6")).toEqual("1+3-4*6");
 	});
-});
-
-xdescribe("prio", function () {
-	it("should return 1+2 for 1+2", function () {
-		expect(prio("1+2")).toEqual("1+2");
-	});
-
-	it("should return 1+2 for 1+2+3", function () {
-		expect(prio("1+2+3")).toEqual("1+2");
-	});
-
-	it("should return 2*3 for 1+2*3", function () {
-		expect(prio("1+2*3")).toEqual("2*3");
-	});
-
-	it("should return 2/3 for 1+2/3", function () {
-		expect(prio("1+2/3")).toEqual("2/3");
-	});
-
-	it("should return 1*2 for 1*2/3", function () {
-		expect(prio("1*2/3")).toEqual("1*2");
-	});
-
-	it("should return 2/3 for 1+2/3*4", function () {
-		expect(prio("1+2/3*4")).toEqual("2/3");
+	it("should replace all white space from expression incl numbers with decimal separator", function () {
+		expect(removeWS("1.5 + 3.045-4  *  6.22")).toEqual("1.5+3.045-4*6.22");
 	});
 });
 
@@ -103,7 +80,6 @@ describe("shoud handle Punkt-vor-Strich", function () {
 });
 
 
-
 describe("reduceOperators should eval operator pairs", function () {
 	it("for 1++1 -> 1+1", function () {
 		expect(reduceOperators("1++1")).toEqual("1+1");
@@ -120,6 +96,7 @@ describe("reduceOperators should eval operator pairs", function () {
 	it("for 1++-1 -> 1-1", function () {
 		expect(reduceOperators("1++-1")).toEqual("1-1");
 	});
+
 	it("for 1+-+1 -> 1-1", function () {
 		expect(reduceOperators("1+-+1")).toEqual("1-1");
 	});
@@ -127,12 +104,152 @@ describe("reduceOperators should eval operator pairs", function () {
 	it("for 1--1 -> 1+1", function () {
 		expect(reduceOperators("1--1")).toEqual("1+1");
 	});
+
 	it("for 1---1 -> 1-1", function () {
 		expect(reduceOperators("1---1")).toEqual("1-1");
+	});
+
+	it("for 1.5---1+-4.0 -> 1.5-1-4.0", function () {
+		expect(reduceOperators("1.5---1+-4.0")).toEqual("1.5-1-4.0");
+	});
+});
+
+
+describe("zeroFill should add 0 to unary operators", function () {
+	it("for +1", function () {
+		expect(zeroFill("+1")).toEqual("0+1");
+	});
+
+	it("for -1", function () {
+		expect(zeroFill("-1")).toEqual("0-1");
+	});
+
+	it("for 1+1+(-4)+5", function () {
+		expect(zeroFill("1+1+(-4)+5")).toEqual("1+1+(0-4)+5");
+	});
+
+	it("for 1++1", function () {
+		expect(zeroFill("1++1")).toEqual("1++1");
+	});
+
+	it("for 1-+1", function () {
+		expect(zeroFill("1-+1")).toEqual("1-+1");
+	});
+
+	it("for -1.5", function () {
+		expect(zeroFill("-1.5")).toEqual("0-1.5");
+	});
+
+	it("for 1.0-+1.5", function () {
+		expect(zeroFill("1.0-+1.5")).toEqual("1.0-+1.5");
+	});
+
+	it("for 1+1+(-4.0)+5", function () {
+		expect(zeroFill("1+1+(-4.0)+5")).toEqual("1+1+(0-4.0)+5");
+	});
+});
+
+describe("should handle exp with unary operators", function () {
+	it("for +1", function () {
+		expect(calc("+1")).toEqual(1);
+	});
+
+	it("for -1", function () {
+		expect(calc("-1")).toEqual(-1);
+	});
+
+	it("for 1++1", function () {
+		expect(calc("1++1")).toEqual(2);
+	});
+
+	it("for 1-+1", function () {
+		expect(calc("1-+1")).toEqual(0);
+	});
+
+});
+
+
+describe("solveParenthesis should handle parenthesis", function () {
+	it("(1+3)", function () {
+		expect(solveParenthesis("(1+3)")).toEqual("4");
+	});
+	it("(-3)", function () {
+		expect(solveParenthesis("(-3)")).toEqual("-3");
+	});
+	it("2*(9-3)", function () {
+		expect(solveParenthesis("2*(9-3)")).toEqual("2*6");
+	});
+	it("2*(9-3)+5", function () {
+		expect(solveParenthesis("2*(9-3)+5")).toEqual("2*6+5");
+	});
+	it("2*(9-3)+5-(4*2+(1+1))", function () {
+		expect(solveParenthesis("2*(9-3)+5-(4*2+(1+1))")).toEqual("2*6+5-10");
+	});
+	it("2*(9-3)+5-(4*2+(1+1))+7", function () {
+		expect(solveParenthesis("2*(9-3)+5-(4*2+(1+1))+7")).toEqual("2*6+5-10+7");
 	});
 });
 
 
 
+describe("calc should handle parenthesis", function () {
+	it("(1+3)", function () {
+		expect(calc("(1+3)")).toEqual(4);
+	});
+	it("(-3)", function () {
+		expect(calc("(-3)")).toEqual(-3);
+	});
+	it("2*(9-3)", function () {
+		expect(calc("2*(9-3)")).toEqual(12);
+	});
+	it("2*(9-3)+5", function () {
+		expect(calc("2*(9-3)+5")).toEqual(17);
+	});
+	it("2*(9-3)+5-(4*2+(1+1))", function () {
+		expect(calc("2*(9-3)+5-(4*2+(1+1))")).toEqual(7);
+	});
+	it("2*(9-3)+5-(4*2+(1+1))+7", function () {
+		expect(calc("2*(9-3)+5-(4*2+(1+1))+7")).toEqual(14);
+	});
+});
+
+describe("calc should handle single numbers", function () {
+	it("42", function () {
+		expect(calc("42")).toEqual(42);
+	});
+	it("--(42)", function () {
+		expect(calc("--(42)")).toEqual(42);
+	});
+	it("--((42))", function () {
+		expect(calc("--((42))")).toEqual(42);
+	});
+});
+
+describe("calc should return null for falsy expressions", function () {
+	it("empty string", function () {
+		expect(calc("")).toEqual(null);
+	});
+	it("null", function () {
+		expect(calc(null)).toEqual(null);
+	});
+	it("undefined", function () {
+		expect(calc(undefined)).toEqual(null);
+	});
+});
+
+describe("calc should handle decimal numbers", function () {
+	it("2.5+4.0", function () {
+		expect(calc("2.5+4.0")).toEqual(6.5);
+	});
+	it("22232.54+456.0", function () {
+		expect(calc("2000.5401+444.1")).toEqual(2444.6401);
+	});
+});
+
+describe("calc should handle devision x / 0", function () {
+	it("5 / 0 should return Infinity", function () {
+		expect(calc("5/0")).toEqual(Infinity);
+	});
+});
 
 
